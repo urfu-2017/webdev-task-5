03ddba54ec7bcffcda1a77414fe867143ef4607d
spt30 <seregatoschilin@gmail.com>
2018-04-01 10:49:24 +0500

    Закомментировал тесты

--- index.js
+++ index.js
@@ -15,23 +15,23 @@ const Queries = require('./queries');
 
     try {
         // Здесь можно делать запросы, чтобы проверять, что они правильно работают
-        const result = await queries.getAllSouvenirs();
-        result = await queries.getCheapSouvenirs(2900);
-        result = await queries.getTopRatingSouvenirs(10);
-        result = await queries.getSouvenirsByTag('техника');
-        result = await queries.getSouvenrisCount(
-            { country: 'Испания',
-                rating: 1,
-                price: 3000 }
-        );
-        result = await queries.searchSouvenirs('рука');
-        result = await queries.getDisscusedSouvenirs(Date());
-        result = await queries.deleteOutOfStockSouvenirs();
-        result = await queries.addReview('5abe65524d0c9d02c12eafb8',
-        { login: 'spt30', rating: '4.2q', text: 'Спасибо, все отлично (нет4)' });
-        result = await queries.getCartSum('punisher');
+        // const result = await queries.getAllSouvenirs();
+        // result = await queries.getCheapSouvenirs(2900);
+        // result = await queries.getTopRatingSouvenirs(10);
+        // result = await queries.getSouvenirsByTag('техника');
+        // result = await queries.getSouvenrisCount(
+        //     { country: 'Испания',
+        //         rating: 1,
+        //         price: 3000 }
+        // );
+        // result = await queries.searchSouvenirs('рука');
+        // result = await queries.getDisscusedSouvenirs(Date());
+        // result = await queries.deleteOutOfStockSouvenirs();
+        // result = await queries.addReview('5abe65524d0c9d02c12eafb8',
+        // { login: 'spt30', rating: '4.2q', text: 'Спасибо, все отлично (нет4)' });
+        // result = await queries.getCartSum('punisher');
 
-        console.log(result);
+        // console.log(result);
 
     } catch (error) {
         console.error(error);
