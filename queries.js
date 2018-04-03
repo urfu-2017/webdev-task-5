6a0f987127927ba2ef34dd215e98ce27f5dd3e4c
Gusarov Vadim <gusarovvadim1996@gmail.com>
2018-04-03 21:42:35 +0500

    done

--- queries.js
+++ queries.js
@@ -1,77 +1,128 @@
 'use strict';
 
+const { Types } = require('mongoose');
+
 module.exports = class Queries {
     constructor(mongoose, { souvenirsCollection, cartsCollection }) {
         const souvenirSchema = mongoose.Schema({ // eslint-disable-line new-cap
-            // Ваша схема сувенира тут
+            tags: { type: [String], required: true },
+            reviews: { type: Array, $items: {
+                id: { type: String, required: true },
+                login: { type: String, required: true },
+                date: { type: Date, required: true },
+                text: { type: String, required: true },
+                rating: { type: Number, required: true },
+                isApproved: { type: Boolean, required: true }
+            }, required: true },
+            name: { type: String, required: true },
+            image: { type: String, required: true },
+            price: { type: Number, required: true },
+            amount: { type: Number, required: true },
+            country: { type: String, required: true },
+            rating: { type: Number, required: true },
+            isRecent: { type: Boolean, required: true }
         });
+
+        souvenirSchema.index({ _id: 1, price: -1, rating: 1 });
 
         const cartSchema = mongoose.Schema({ // eslint-disable-line new-cap
-            // Ваша схема корзины тут
+            items: { type: Array, $items: {
+                souvenirId: { type: mongoose.Types.ObjectId, required: true },
+                amount: { type: Number, required: true }
+            }, required: true },
+            login: { type: String, required: true, unique: true }
         });
 
-        // Модели в таком формате нужны для корректного запуска тестов
+        cartSchema.index({ login: 1 });
+
         this._Souvenir = mongoose.model('Souvenir', souvenirSchema, souvenirsCollection);
         this._Cart = mongoose.model('Cart', cartSchema, cartsCollection);
     }
 
-    // Далее идут методы, которые вам необходимо реализовать:
-
     getAllSouvenirs() {
-        // Данный метод должен возвращать все сувениры
+
+        return this._Souvenir
+            .find({});
     }
 
     getCheapSouvenirs(price) {
-        // Данный метод должен возвращать все сувениры, цена которых меньше или равна price
+
+        return this._Souvenir
+            .find({ price: { $lte: price } });
     }
 
     getTopRatingSouvenirs(n) {
-        // Данный метод должен возвращать топ n сувениров с самым большим рейтингом
+
+        return this._Souvenir
+            .find()
+            .sort({ rating: -1 })
+            .limit(n);
     }
 
     getSouvenirsByTag(tag) {
-        // Данный метод должен возвращать все сувениры, в тегах которых есть tag
-        // Кроме того, в ответе должны быть только поля name, image и price
+
+        return this._Souvenir
+            .find({ tags: tag }, { _id: 0, name: 1, image: 1, price: 1 });
     }
 
     getSouvenrisCount({ country, rating, price }) {
-        // Данный метод должен возвращать количество сувениров,
-        // из страны country, с рейтингом больше или равной rating,
-        // и ценой меньше или равной price
 
-        // ! Важно, чтобы метод работал очень быстро,
-        // поэтому учтите это при определении схем
+        return this._Souvenir
+            .count({ country: country, rating: { $gte: rating }, price: { $lte: price } });
     }
 
     searchSouvenirs(substring) {
-        // Данный метод должен возвращать все сувениры, в название которых входит
-        // подстрока substring. Поиск должен быть регистронезависимым
+
+        return this._Souvenir
+            .find({ 'name': { $regex: new RegExp(substring, 'i') } });
+
     }
 
     getDisscusedSouvenirs(date) {
-        // Данный метод должен возвращать все сувениры,
-        // первый отзыв на которые был оставлен не раньше даты date
+
+        return this._Souvenir
+            .find({ 'reviews.0.date': { $gte: date } });
     }
 
     deleteOutOfStockSouvenirs() {
-        // Данный метод должен удалять все сувениры, которых нет в наличии
-        // (то есть amount = 0)
 
-        // Метод должен возвращать объект формата { ok: 1, n: количество удаленных сувениров }
-        // в случае успешного удаления
+        return this._Souvenir
+            .remove({ amount: 0 });
     }
 
     async addReview(souvenirId, { login, rating, text }) {
-        // Данный метод должен добавлять отзыв к сувениру souvenirId, отзыв добавляется
-        // в конец массива (чтобы сохранить упорядоченность по дате),
-        // содержит login, rating, text - из аргументов,
-        // date - текущая дата и isApproved - false
-        // Обратите внимание, что при добавлении отзыва рейтинг сувенира должен быть пересчитан
+        const souvenir = await this._Souvenir
+            .find({ _id: new Types.ObjectId(souvenirId) }, { _id: 0, reviews: 1 });
+        let sum = rating;
+        souvenir[0].reviews.forEach(review => {
+            sum += review.rating;
+        });
+        let newRating = sum / souvenir[0].reviews.length;
+        await this._Souvenir.update(
+            { _id: new Types.ObjectId(souvenirId) },
+            { $set: { rating: newRating }, $push: { reviews: {
+                login: login,
+                date: new Date(),
+                text: text,
+                rating: rating,
+                isApproved: false
+            } } }
+        );
     }
 
     async getCartSum(login) {
-        // Данный метод должен считать общую стоимость корзины пользователя login
-        // У пользователя может быть только одна корзина, поэтому это тоже можно отразить
-        // в схеме
+        let cart = await this._Cart.find({ login: login });
+        var sum = 0;
+        var promises = [];
+        for (var i = 0; i < cart[0].items.length; i++) {
+            promises.push(this._Souvenir.find({ _id: cart[0].items[i].souvenirId }, { price: 1 }));
+        }
+        await Promise.all(promises).then(values => {
+            values.forEach(value => {
+                sum += value[0].price;
+            });
+        });
+
+        return sum;
     }
 };
