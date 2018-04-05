62345c1f5f6f960a24e42e1ce511137a298f3768
DorokhovaTatiana <t.dorohovav@gmail.com>
2018-04-05 17:57:11 +0500

    lint

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ const Queries = require('./queries');
 
     try {
         // Здесь можно делать запросы, чтобы проверять, что они правильно работают
-        const result = await queries.addReview('5abe65524d0c9d02c12eafb7', { login: 'bhh', rating: 3, text: 'jjjjj' });
         // const result = await queries.getCartSum('justice');
         // const result = await queries.searchSouvenirs('И');
-        console.info(result);
+        // console.info(result);
     } catch (error) {
         console.error(error);
     }
