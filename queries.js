51b210f32b56fd9830fcb99ec1a74c784a1b64f3
ToshchevIvan <toshchev.ivan@gmail.com>
2018-04-06 16:04:23 +0500

    -

--- queries.js
+++ queries.js
@@ -3,17 +3,9 @@
 
 module.exports = class Queries {
     constructor(mongoose, { souvenirsCollection, cartsCollection }) {
-        const reviewSchema = mongoose.Schema({ // eslint-disable-line new-cap
-            login: String,
-            date: Date,
-            text: String,
-            rating: Number,
-            isApproved: Boolean
-        });
-
         const souvenirSchema = mongoose.Schema({ // eslint-disable-line new-cap
             tags: [String],
-            reviews: [reviewSchema],
+            reviews: [mongoose.Schema.Types.Mixed],
             name: String,
             image: String,
             price: { type: Number, index: true },
