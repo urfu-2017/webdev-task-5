24f6afc75b746bf412a4703cfcce0b1a2e3ae8fd
San4eyZ <sanya.rogov37@gmail.com>
2018-04-09 00:35:28 +0500

    Альфа

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const mongoose = require('mongoose');
 const Queries = require('./queries');
 
@@ -11,12 +13,23 @@ const Queries = require('./queries');
 
     try {
         // Здесь можно делать запросы, чтобы проверять, что они правильно работают
-        const result = await queries.getAllSouvenirs();
-
-        console.log(result);
+        // const result = await queries.getAllSouvenirs();
+        // const result = await queries.getCheapSouvenirs(100);
+        // const result = await queries.getTopRatingSouvenirs(10);
+        // const result = await queries.getSouvenirsByTag('магнит');
+        // const result = await queries.searchSouvenirs('РАКУШКА');
+        // const result = await queries.getDisscusedSouvenirs(new Date(0));
+        // const result = await queries.deleteOutOfStockSouvenirs();
+        // const result = await queries.getCartSum('punisher');
+        const result = await queries.addReview('5abe65514d0c9d02c12eaf55', {
+            login: 'batman',
+            text: 'asdfsdfasdfasdfasdfasdf',
+            rating: 5
+        });
+        console.info(result);
     } catch (error) {
         console.error(error);
     }
 
     await mongoose.disconnect();
-})();
+}());
