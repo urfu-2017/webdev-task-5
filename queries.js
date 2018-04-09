bb9088479979dc15cc9f70a6bf48c294e45069c1
d.fomin <d.fomin@skbkontur.ru>
2018-04-09 15:40:49 +0500

    test

--- queries.js
+++ queries.js
@@ -75,8 +75,7 @@ module.exports = class Queries {
     async searchSouvenirs(substring) {
         // Данный метод должен возвращать все сувениры, в название которых входит
         // подстрока substring. Поиск должен быть регистронезависимым
-        return [substring];
-        // return await this._Souvenir.find({ 'name': { '$regex': `^.*${substring}.*$` } });
+         return await this._Souvenir.find({ 'name': { '$regex': substring, "$options": "i" } });
     }
 
     async getDisscusedSouvenirs(date) {
